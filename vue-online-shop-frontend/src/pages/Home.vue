c4e23530e7fef9f5813b372148cebdf306ec3bec
Aucdmn <auop129@163.com>
2020-09-07 11:23:41 +0800

    完成第一部分页面导航及跳转

---
+++ vue-online-shop-frontend/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <div class="title">
+         <h1>{{msg}}</h1>
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
